2b4644a913214a4bf44d85cbb492d9d96b132ddc
Todd Bonner <toddkbonner@yahoo.com>
2025-02-26 10:50:37 -0500

    Create convert.py
    Created script using public documents folder as a default directory

---
+++ convert.py
@@ -0,0 +1,21 @@
+#convert script
+import os
+from fontTools.ttLib import TTFont
+
+def convert_ttf_to_woff(ttf_path, woff_path):
+    font = TTFont(ttf_path)
+    font.flavor = 'woff'
+    font.save(woff_path)
+
+def convert_all_ttf_in_directory(directory):
+    for filename in os.listdir(directory):
+        if filename.endswith(".ttf"):
+            ttf_path = os.path.join(directory, filename)
+            woff_path = os.path.join(directory, filename.replace(".ttf", ".woff"))
+            convert_ttf_to_woff(ttf_path, woff_path)
+            print(f"Converted {ttf_path} to {woff_path}")
+
+# Example usage
+# just change the path to the path you desire. 
+directory = 'C:\Users\Public\Documents\ttf'
+convert_all_ttf_in_directory(directory)
