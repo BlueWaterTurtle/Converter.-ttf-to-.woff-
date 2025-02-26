cb9c394bb4f6d55e38e8191ae5fda326bbe12e97
Todd Bonner <toddkbonner@yahoo.com>
2025-02-26 10:56:58 -0500

    Update convert.py
    added a debug message

--- convert.py
+++ convert.py
@@ -12,10 +12,13 @@ def convert_all_ttf_in_directory(directory):
         if filename.endswith(".ttf"):
             ttf_path = os.path.join(directory, filename)
             woff_path = os.path.join(directory, filename.replace(".ttf", ".woff"))
-            convert_ttf_to_woff(ttf_path, woff_path)
-            print(f"Converted {ttf_path} to {woff_path}")
+            try:
+                convert_ttf_to_woff(ttf_path, woff_path)
+                print(f"Converted {ttf_path} to {woff_path}")
+            except Exception as e:
+                print(f"Failed to convert{ttf_path}: {e}")
 
 # Example usage
 # just change the path to the path you desire. 
-directory = 'C:\Users\Public\Documents\ttf'
+directory = 'C:\\Users\\Public\Documents\\ttf'
 convert_all_ttf_in_directory(directory)
