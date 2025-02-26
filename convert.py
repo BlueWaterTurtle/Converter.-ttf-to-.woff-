cd03ac5fc756ee9806d12a6c0ca33616e5400149
Todd Bonner <toddkbonner@yahoo.com>
2025-02-26 11:19:24 -0500

    Update convert.py
    Test Branch

--- convert.py
+++ convert.py
@@ -16,12 +16,12 @@ def convert_all_ttf_in_directory(directory):
                 convert_ttf_to_woff(ttf_path, woff_path)
                 print(f"Converted {ttf_path} to {woff_path}")
             except Exception as e:
-                print(f"Failed to convert{ttf_path}: {e}")
+                print(f"Failed to convert {ttf_path}: {e}")
 
 # Example usage
-# just change the path to the path you desire. 
+# just change the path to the path you desire.
 directory = r'C:\Users\Public\Documents\ttf'
 convert_all_ttf_in_directory(directory)
 
-# Keep the window open # I'll probably comment this line out after I get the script working
+# Keep the window open
 input("Press Enter to exit...")
